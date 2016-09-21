f8eed465ec26bd1d5c40c79b37521795101ddf26
lenbo-ma <mlongbo@gmail.com>
2016-09-21 11:56:04 +0800

    add test action

--- src/main/java/com/mlongbo/sunflower/server/example/controller/UserController.java
+++ src/main/java/com/mlongbo/sunflower/server/example/controller/UserController.java
@@ -1,9 +1,29 @@
 package com.mlongbo.sunflower.server.example.controller;
 
 import com.mlongbo.sunflower.server.BaseController;
+import com.mlongbo.sunflower.server.Mapping;
+import com.mlongbo.sunflower.server.Method;
+import com.mlongbo.sunflower.server.MethodType;
 
 /**
  * @author malongbo
  */
 public class UserController  extends BaseController{
+
+    /**
+     * 查询用户信息接口
+     */
+    @Method(MethodType.GET)
+    public void profile() {
+
+    }
+
+    /**
+     * 更新用户信息接口
+     */
+    @Mapping("profile")
+    @Method(MethodType.PUT)
+    public void updateProfile() {
+
+    }
 }
